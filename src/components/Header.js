d67678601b6ae0d9bd4db55e16006ea9b8c1791d
Kazuma <kazuma3773k@gmail.com>
2025-08-30 11:52:02 +0900

    feat: ブログアプリの修正

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Bar from "./Bar";
+
+
+function Header ()  {
+    return(
+        <>
+        <Bar />
+        <h1></h1>
+        </>
+    );
+}
+
+export default Header;
